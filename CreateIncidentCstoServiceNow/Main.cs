c151cdb30fa18c78c537c784d73d3301cef61785
Laurens Brand <laurens.brand@me.com>
2017-08-05 20:42:48 +0200

    Created the arguments

--- CreateIncidentCstoServiceNow/Main.cs
+++ CreateIncidentCstoServiceNow/Main.cs
@@ -5,31 +5,43 @@ using System.Text;
 namespace CreateIncidentCstoServiceNow
 {
 	class CreateIncident
-	{
-		private const string URL = "https://dev26100.service-now.com/api/now/table/incident";
-		private const string DATA = @"{
-            ""short_description"": ""Alloha"",
-            ""comments"": ""This is a very serious incident""
-            }";
+    {
+        private const int ARGS_LENGTH = 5;
 
 		static void Main(string[] args)
 		{
-			CreateIncidentTicket();
+			string URL = "https://" + args[0] + ".service-now.com/api/now/v1/table/incident";
+			string USERNAME = args[1];
+			string PASSWORD = args[2];
+            string SHORT_DESCRIPTION = args[3];
+            string COMMENTS = args[4];
+					
+            CreateIncidentTicket(URL,USERNAME,PASSWORD,SHORT_DESCRIPTION,COMMENTS);
 		}
 
-		private static void CreateIncidentTicket()
+		private static void CreateIncidentTicket(string URL, string USERNAME, string PASSWORD, string SHORT_DESCRIPTION, string COMMENTS)
 		{
-            Console.WriteLine("Start REST API Call");
+            /*
+            string DATA = @"{
+            ""short_description"": ""{0}"",
+            ""comments"": ""{1}""
+            }";
+            */
+			string DATA = @"{
+            ""short_description"": ""{0}"",
+            ""comments"": ""{1}""
+            }";
+
 			System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
 			client.BaseAddress = new System.Uri(URL);
-			byte[] cred = UTF8Encoding.UTF8.GetBytes("admin1:YYtYYt01");
+			byte[] cred = UTF8Encoding.UTF8.GetBytes("admin:YYtYYt01");
 			client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(cred));
 			client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 
 			System.Net.Http.HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
 			HttpResponseMessage messge = client.PostAsync(URL, content).Result;
 			string description = string.Empty;
-            Console.WriteLine("Result code is {0}",messge.IsSuccessStatusCode);
+            Console.WriteLine("Result code is {0} with message {1}",messge.IsSuccessStatusCode, messge.StatusCode);
 			if (messge.IsSuccessStatusCode)
 			{
 				string result = messge.Content.ReadAsStringAsync().Result;
